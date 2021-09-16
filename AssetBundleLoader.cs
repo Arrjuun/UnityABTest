780fd39ce3f2bc3232db11f3a621aa39edf20565
Arjun C <a.c@uq.net.au>
2021-09-16 17:33:21 +0530

    Updated generalization

--- AssetBundleLoader.cs
+++ AssetBundleLoader.cs
@@ -4,70 +4,98 @@ using System.Reflection;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class AssetBundleLoader : MonoBehaviour
 {
     public string assemblyUrl = "http://localhost/assets/assembly_for_assetbundle.dll";
-    public string bundleUrl = "http://localhost/assets/myassetbundle.unity3d";
-    public Text loggingText;
+    public string bundleUrl = "http://localhost/assets/myassetbundle";
 
-    void Start()
+    public bool localPath = true;
+
+    public string dllFileName = "";
+    public string assetBundleFileName = "";
+
+    AssetBundle loadedAssetBundle;
+    byte[] dllData;
+
+    public void OnClickLoadScene()
     {
         StartCoroutine(LoadBundledScene());
     }
 
+    void LoadAssetBundleScene()
+    {
+        if(dllData != null)
+        {
+            Assembly a = Assembly.Load(dllData);
+        }
+
+        string[] scenePath = loadedAssetBundle.GetAllScenePaths();
+
+        SceneManager.LoadScene(scenePath[0]);
+    }
+
     IEnumerator LoadBundledScene()
     {
-        UnityWebRequest dllWebRequest = UnityWebRequest.Get(assemblyUrl);
-        UnityWebRequest assetBundleWebRequest = UnityWebRequestAssetBundle.GetAssetBundle(bundleUrl);
-
-        loggingText.text += "Starting dll download\n";
-        yield return dllWebRequest.SendWebRequest();
-
-        if (dllWebRequest.result != UnityWebRequest.Result.Success)
+        if (localPath)
         {
-            Debug.Log(dllWebRequest.error);
-            loggingText.text += "Error getting dll\n";
+            if (!dllFileName.Equals(""))
+            {
+                string filePath = Path.Combine(Application.streamingAssetsPath, dllFileName);
+                if (filePath.Contains("://") || filePath.Contains(":///"))
+                {
+                    
+                    UnityWebRequest assetBundleRequest = UnityWebRequest.Get(filePath);
+                    yield return assetBundleRequest.SendWebRequest();
+
+                    if(assetBundleRequest.result == UnityWebRequest.Result.Success)
+                    {
+                        dllData = assetBundleRequest.downloadHandler.data;
+                    }
+                }
+                else
+                {
+                    dllData = File.ReadAllBytes(filePath);
+                }
+            }
+
+            loadedAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, assetBundleFileName));
         }
         else
         {
-            Debug.Log("Successfully downloaded dll");
-            loggingText.text += "Successfully downloaded dll\n";
-            byte[] dllData = dllWebRequest.downloadHandler.data;
-            Assembly a = Assembly.Load(dllData);
-            var m = a.Modules;
-            foreach(var ma in m)
+            if(!assemblyUrl.Equals(""))
             {
-                loggingText.text += $"{ma.FullyQualifiedName}\n";
+                UnityWebRequest dllWebRequest = UnityWebRequest.Get(assemblyUrl);
+
+                yield return dllWebRequest.SendWebRequest();
+
+                if (dllWebRequest.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.Log(dllWebRequest.error);
+                }
+                else
+                {
+                    Debug.Log("Successfully downloaded dll");
+                    dllData = dllWebRequest.downloadHandler.data;
+                }
+            }
+
+            UnityWebRequest assetBundleWebRequest = UnityWebRequestAssetBundle.GetAssetBundle(bundleUrl);
+
+            yield return assetBundleWebRequest.SendWebRequest();
+
+            if (assetBundleWebRequest.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(assetBundleWebRequest.error);
+            }
+            else
+            {
+                Debug.Log("Successfully downloaded Asset bundle");
+                loadedAssetBundle = DownloadHandlerAssetBundle.GetContent(assetBundleWebRequest);
             }
         }
 
-        loggingText.text += "Starting asset bundle download\n";
-        yield return assetBundleWebRequest.SendWebRequest();
-
-        if (assetBundleWebRequest.result != UnityWebRequest.Result.Success)
-        {
-            Debug.Log(assetBundleWebRequest.error);
-            loggingText.text += "Error getting asset bundle\n";
-        }
-        else
-        {
-            Debug.Log("Successfully downloaded Asset bundle");
-            loggingText.text += "Successfully downloaded assetbundle\n";
-            AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(assetBundleWebRequest);
-
-            string[] scenePath = bundle.GetAllScenePaths();
-            //Debug.Log(scenePath.Length);
-
-            foreach (var path in scenePath)
-            {
-                loggingText.text += $"{path}\n";
-            }
-
-            SceneManager.LoadSceneAsync(scenePath[0]);
-
-            //Instantiate(bundle.LoadAsset(assetName));
-            //bundle.Unload(false);
-        }
+        LoadAssetBundleScene();
     }
 }
