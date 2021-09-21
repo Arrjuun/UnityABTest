956fb4c34a244eb52624f74801fad815e91c80d5
Arjun C <a.c@uq.net.au>
2021-09-21 14:14:17 +0530

    Saving assetbundle on file system and loading

--- AssetBundleLoader.cs
+++ AssetBundleLoader.cs
@@ -11,6 +11,8 @@ public class AssetBundleLoader : MonoBehaviour
     public string assemblyUrl = "http://localhost/assets/assembly_for_assetbundle.dll";
     public string bundleUrl = "http://localhost/assets/myassetbundle";
 
+    public string remoteAssetBundleFileName = "assetbundle";
+
     public bool localPath = true;
 
     public string dllFileName = "";
@@ -19,6 +21,39 @@ public class AssetBundleLoader : MonoBehaviour
     AssetBundle loadedAssetBundle;
     byte[] dllData;
 
+    public uint version = 1;
+    //public uint crc;
+
+    public string modelId = "5b770936-255e-49b8-9473-c0cf44494155";
+
+    public Image loadingImage;
+
+    public bool isDownloadedOnFileSystem = false;
+
+    string projectBundlePath;
+    string downloadedBundlesPath;
+
+    private void Start()
+    {
+        projectBundlePath = Path.Combine(Application.persistentDataPath, "ProjectBundles");
+        downloadedBundlesPath = Path.Combine(Application.persistentDataPath, "DownloadedBundles");
+
+        if (!Directory.Exists(projectBundlePath))
+        {
+            Directory.CreateDirectory(projectBundlePath);
+        }
+
+        if (!Directory.Exists(downloadedBundlesPath))
+        {
+            Directory.CreateDirectory(downloadedBundlesPath);
+        }
+
+        Cache currentCache = Caching.AddCache(projectBundlePath);
+        Caching.currentCacheForWriting = currentCache;
+        Debug.Log($"Current Cache for writing : {Caching.currentCacheForWriting.path}");
+
+    }
+
     public void OnClickLoadScene()
     {
         StartCoroutine(LoadBundledScene());
@@ -26,7 +61,7 @@ public class AssetBundleLoader : MonoBehaviour
 
     void LoadAssetBundleScene()
     {
-        if(dllData != null)
+        if (dllData != null)
         {
             Assembly a = Assembly.Load(dllData);
         }
@@ -45,11 +80,11 @@ public class AssetBundleLoader : MonoBehaviour
                 string filePath = Path.Combine(Application.streamingAssetsPath, dllFileName);
                 if (filePath.Contains("://") || filePath.Contains(":///"))
                 {
-                    
+
                     UnityWebRequest assetBundleRequest = UnityWebRequest.Get(filePath);
                     yield return assetBundleRequest.SendWebRequest();
 
-                    if(assetBundleRequest.result == UnityWebRequest.Result.Success)
+                    if (assetBundleRequest.result == UnityWebRequest.Result.Success)
                     {
                         dllData = assetBundleRequest.downloadHandler.data;
                     }
@@ -64,11 +99,63 @@ public class AssetBundleLoader : MonoBehaviour
         }
         else
         {
-            if(!assemblyUrl.Equals(""))
+            string dir = Path.Combine(downloadedBundlesPath, $"{modelId}_{version}");
+            Debug.Log(dir);
+
+            if (!Directory.Exists(dir))
+            {
+                Debug.Log("Creating Directory");
+                Directory.CreateDirectory(dir);
+            }
+
+            if (File.Exists(Path.Combine(dir, remoteAssetBundleFileName)))
+            {
+                bundleUrl = Path.Combine(dir, remoteAssetBundleFileName);
+                isDownloadedOnFileSystem = true;
+            }
+            else
+            {
+                UnityWebRequest downloadAssetBundleRequest = UnityWebRequest.Get(bundleUrl);
+
+                var downloadAssetBundleOperation = downloadAssetBundleRequest.SendWebRequest();
+
+                float downloadAssetBundleProgress = 0;
+
+                while (!downloadAssetBundleOperation.isDone)
+                {
+                    downloadAssetBundleProgress = downloadAssetBundleRequest.downloadProgress * 100;
+
+                    Debug.Log("Download: " + downloadAssetBundleProgress);
+                    yield return null;
+                }
+
+                if (downloadAssetBundleRequest.result != UnityWebRequest.Result.Success)
+                {
+
+                }
+                else
+                {
+                    File.WriteAllBytes(Path.Combine(dir, remoteAssetBundleFileName), downloadAssetBundleRequest.downloadHandler.data);
+                    bundleUrl = Path.Combine(dir, remoteAssetBundleFileName);
+                }
+            }
+
+            if (!assemblyUrl.Equals(""))
             {
                 UnityWebRequest dllWebRequest = UnityWebRequest.Get(assemblyUrl);
 
-                yield return dllWebRequest.SendWebRequest();
+                var dllDownloadOperation = dllWebRequest.SendWebRequest();
+                float dllDownloadDataProgress;
+
+                while (!dllDownloadOperation.isDone)
+                {
+                    dllDownloadDataProgress = dllWebRequest.downloadProgress * 100;
+
+                    Debug.Log("Download: " + dllDownloadDataProgress);
+                    yield return null;
+                }
+
+                Debug.Log($"Download Progress : {dllWebRequest.downloadProgress * 100}");
 
                 if (dllWebRequest.result != UnityWebRequest.Result.Success)
                 {
@@ -81,9 +168,22 @@ public class AssetBundleLoader : MonoBehaviour
                 }
             }
 
+            Debug.Log($"Loading Asset Bundle from {bundleUrl}");
+
             UnityWebRequest assetBundleWebRequest = UnityWebRequestAssetBundle.GetAssetBundle(bundleUrl);
 
-            yield return assetBundleWebRequest.SendWebRequest();
+            var assetBundleDownloadOperation = assetBundleWebRequest.SendWebRequest();
+            float assetBundleDownloadDataProgress;
+
+            while (!assetBundleDownloadOperation.isDone)
+            {
+                assetBundleDownloadDataProgress = assetBundleWebRequest.downloadProgress * 100;
+
+                Debug.Log("Loading : " + assetBundleDownloadDataProgress);
+                yield return null;
+            }
+
+            Debug.Log($"Loading Progress : {assetBundleWebRequest.downloadProgress * 100}");
 
             if (assetBundleWebRequest.result != UnityWebRequest.Result.Success)
             {
